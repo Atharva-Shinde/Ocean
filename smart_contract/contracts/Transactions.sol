21169663b6878a2088a18f9802d57873058b56b4
Atharva Shinde <atharvashinde179@gmail.com>
2022-02-11 20:07:49 +0530

    created: smart contract

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Transactions {
+    uint256 transactionCount;
+
+    event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
+
+    // the structure of object TransferStruct 
+    struct TransferStruct {
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    // an array of transactions 
+    TransferStruct[] transactions;
+
+    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
+        transactionCount +=1;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
+
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
+    }
+
+    // "view" means we cannot modify the blockchain
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
+        // returns an array of transactions 
+        return transactions;
+    }
+
+    function getAllTransactionsCount() public view returns (uint256){
+        // returns transactioncounts as uint256 as TransactionCount variable declared above
+        return transactionCount;
+    }
+}
