28cfc0365ef6deb8b5b83093e40182797e6f6b37
Atharva Shinde <atharvashinde179@gmail.com>
2022-02-13 03:39:17 +0530

    created: utilities foleder to connect smart-contract to the client side

---
+++ client/src/utilities/constants.js
@@ -0,0 +1,6 @@
+// This Transactions.json is copy-pasted from smart_contract/artifacts/Transactions/Transactions.json
+// it contains all the information of our smart contract
+import abi from './Transactions.json';
+
+export const contractAddress ='0x98a4b1E5B64C2a5Da49458C42079737C0B933b86';
+export const contractABI = abi.abi;
